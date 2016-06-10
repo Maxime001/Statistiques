52a9743073bcd6af11aced63baeeebd99e2d466a
MaximeS <maxime.simet@yahoo.fr>
2016-06-10 10:26:53 +0200

    ajax

--- index.php
+++ index.php
@@ -5,10 +5,14 @@
 <!doctype html>
 <html>
     <head>
-        
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+        <script src="js/ajax.js"></script>
+        <script src="js/main.js"></script>
+
     </head>
     
     <body>
-        
+        <button>Mise à jour des données</button>
+        <div id="status"></div>
     </body>
 </html>
