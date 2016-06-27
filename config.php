4e2b8516b8262ff46c857f292ffd837ad8ac879f
nicolasuha40 <nicolas.ambert@gmail.com>
2016-06-27 16:34:02 +0200

    ajout de la partie recherche dans la bdd

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+global $hostname;
+$hostname ="localhost";
+
+global $username ;
+$username = "root";
+
+global $password;
+$password = "uha40";
+
+global $namebase;
+$namebase = "statistiques";
+
+// ne pas oublier les droits lors de la creaction de ce fichier !
+//sudo chown www-data donneesTest_v2.json
+global $nomFichierJSON;
+//$nomFichierJSON = "js/donneesTest.json";
+$nomFichierJSON = "js/donneesTest.json";
+
+
+?>
